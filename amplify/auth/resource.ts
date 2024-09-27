cd38bcaacb30272f7f12582e4a5d19b741a97f2d
Nic Wanavit <nwanavit@gmail.com>
2024-09-27 10:32:29 +0700

    rollback to google

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,12 +8,12 @@ export const auth = defineAuth({
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
       },
-      signInWithApple: {
-        clientId: secret("SIWA_CLIENT_ID"),
-        keyId: secret("SIWA_KEY_ID"),
-        privateKey: secret("SIWA_PRIVATE_KEY"),
-        teamId: secret("SIWA_TEAM_ID"),
-      },
+      // signInWithApple: {
+      //   clientId: secret("SIWA_CLIENT_ID"),
+      //   keyId: secret("SIWA_KEY_ID"),
+      //   privateKey: secret("SIWA_PRIVATE_KEY"),
+      //   teamId: secret("SIWA_TEAM_ID"),
+      // },
       // loginWithAmazon: {
       //   clientId: secret("LOGINWITHAMAZON_CLIENT_ID"),
       //   clientSecret: secret("LOGINWITHAMAZON_CLIENT_SECRET"),
