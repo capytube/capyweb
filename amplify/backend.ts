88a6a25116a96f7794871b6061c2ef7134f64335
Nic Wanavit <nwanavit@gmail.com>
2024-09-27 11:39:29 +0700

    add function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { getStream } from "./functions/getStream/resource";
 
 defineBackend({
   auth,
   data,
+  getStream,
 });
