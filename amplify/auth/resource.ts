c9c40fbe31ea68dc6ccb3a81a612d2b26ce5fbf2
Nic Wanavit <nwanavit@gmail.com>
2024-10-01 12:57:00 +0700

    try just google first

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,16 +8,16 @@ export const auth = defineAuth({
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
       },
-      signInWithApple: {
-        clientId: secret("SIWA_CLIENT_ID"),
-        keyId: secret("SIWA_KEY_ID"),
-        privateKey: secret("SIWA_PRIVATE_KEY"),
-        teamId: secret("SIWA_TEAM_ID"),
-      },
-      facebook: {
-        clientId: secret("FACEBOOK_CLIENT_ID"),
-        clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
-      },
+      // signInWithApple: {
+      //   clientId: secret("SIWA_CLIENT_ID"),
+      //   keyId: secret("SIWA_KEY_ID"),
+      //   privateKey: secret("SIWA_PRIVATE_KEY"),
+      //   teamId: secret("SIWA_TEAM_ID"),
+      // },
+      // facebook: {
+      //   clientId: secret("FACEBOOK_CLIENT_ID"),
+      //   clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
+      // },
       callbackUrls: [
         "http://localhost:5137/profile",
         "https://capytube.xyz/profile",
