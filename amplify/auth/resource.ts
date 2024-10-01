b1b283aec9f55837775890283fc9df13a741f29a
Nic Wanavit <nwanavit@gmail.com>
2024-10-01 13:17:26 +0700

    just email for google

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,7 +7,7 @@ export const auth = defineAuth({
       google: {
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-        scopes: ["public_profile", "email"],
+        scopes: ["email"],
       },
       // signInWithApple: {
       //   clientId: secret("SIWA_CLIENT_ID"),
