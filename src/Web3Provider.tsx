186e49b059fef38b063b7b1f13e43facd942e193
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2024-10-15 13:56:23 +0700

    add ConnectKit

---
+++ src/Web3Provider.tsx
@@ -0,0 +1,32 @@
+import { WagmiProvider, createConfig } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [baseSepolia],
+
+    walletConnectProjectId: "5b600b13f39b789df2c8c85f41209a40",
+
+    // Required App Info
+    appName: "CapyTube",
+
+    // Optional App Info
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
