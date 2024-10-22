6590377436a679d76e2953917d0fbd1595c6296e
Nic Wanavit <nwanavit@gmail.com>
2024-10-22 16:40:54 +0700

    remove authentication

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import PrivacyPolicy from "./components/PrivacyPolicy";
 import TermsOfService from "./components/TermsOfService";
 import DeletionInstructions from "./components/DeletionInstructions"; // Import the Deletion component
 import ProfilePage from "./components/ProfilePage"; // Import the Profile Page component
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import "./App.css";
 import StreamingHome from "./components/Streaming/StreamingHome";
-import SignOutButton from "./components/SignOutButton"; // Import the SignOutButton
 import Logo from "./components/Logo";
 import AboutMagnus from "./components/AboutMagnus";
 import FullScreenStream from "./components/Streaming/FullScreenStream";
@@ -47,83 +45,67 @@ const styles = {
 const App: React.FC = () => {
   return (
     <Web3Provider>
-      <Authenticator>
-        {({ signOut, user }) => (
-          <Router>
-            <main>
-              <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-                <header className="bg-gray-800 p-6 rounded-lg shadow-md text-white max-w-3xl w-11/12">
-                  <div style={styles.container}>
-                    <h1 className="text-4xl mb-5">CapyTube</h1>
-                    <div style={styles.rightbar}>
-                      <div style={styles.logoAndSignout}>
-                        <Logo />
-                        <SignOutButton signOut={signOut} />
-                        <ConnectKitButton />
-                      </div>
-                      <p>{user?.signInDetails?.loginId}</p>
-                    </div>
+      <Router>
+        <main>
+          <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <header className="bg-gray-800 p-6 rounded-lg shadow-md text-white max-w-3xl w-11/12">
+              <div style={styles.container}>
+                <h1 className="text-4xl mb-5">CapyTube</h1>
+                <div style={styles.rightbar}>
+                  <div style={styles.logoAndSignout}>
+                    <Logo />
+                    <ConnectKitButton />
                   </div>
-                  <nav className="mb-4">
-                    <Link
-                      to="/"
-                      className="mr-5 text-blue-300 hover:text-blue-500"
-                    >
-                      Home
-                    </Link>
-                    <Link
-                      to="/profile"
-                      className="mr-5 text-blue-300 hover:text-blue-500"
-                    >
-                      Profile
-                    </Link>
-                    <Link
-                      to="/privacy-policy"
-                      className="mr-5 text-blue-300 hover:text-blue-500"
-                    >
-                      Privacy Policy
-                    </Link>
-                    <Link
-                      to="/terms-of-service"
-                      className="mr-5 text-blue-300 hover:text-blue-500"
-                    >
-                      Terms of Service
-                    </Link>
-                    <Link
-                      to="/about-magnus"
-                      className="mr-5 text-blue-300 hover:text-blue-500"
-                    >
-                      About
-                    </Link>
-                  </nav>
-
-                  {/* Define Routes */}
-                  <Routes>
-                    <Route path="/" element={<StreamingHome />} />
-                    {/* Full screen stream page */}
-                    <Route
-                      path="/stream/:streamId/:streamTitle"
-                      element={<FullScreenStream />}
-                    />
-                    <Route path="/profile" element={<ProfilePage />} />{" "}
-                    {/* Add profile route */}
-                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                    <Route
-                      path="/terms-of-service"
-                      element={<TermsOfService />}
-                    />
-                    <Route
-                      path="/deletion"
-                      element={<DeletionInstructions />}
-                    />
-                    <Route path="/about-magnus" element={<AboutMagnus />} />
-                  </Routes>
-                </header>
+                </div>
               </div>
-            </main>
-          </Router>
-        )}
-      </Authenticator>
+              <nav className="mb-4">
+                <Link to="/" className="mr-5 text-blue-300 hover:text-blue-500">
+                  Home
+                </Link>
+                <Link
+                  to="/profile"
+                  className="mr-5 text-blue-300 hover:text-blue-500"
+                >
+                  Profile
+                </Link>
+                <Link
+                  to="/privacy-policy"
+                  className="mr-5 text-blue-300 hover:text-blue-500"
+                >
+                  Privacy Policy
+                </Link>
+                <Link
+                  to="/terms-of-service"
+                  className="mr-5 text-blue-300 hover:text-blue-500"
+                >
+                  Terms of Service
+                </Link>
+                <Link
+                  to="/about-magnus"
+                  className="mr-5 text-blue-300 hover:text-blue-500"
+                >
+                  About
+                </Link>
+              </nav>
+
+              {/* Define Routes */}
+              <Routes>
+                <Route path="/" element={<StreamingHome />} />
+                {/* Full screen stream page */}
+                <Route
+                  path="/stream/:streamId/:streamTitle"
+                  element={<FullScreenStream />}
+                />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                <Route path="/terms-of-service" element={<TermsOfService />} />
+                <Route path="/deletion" element={<DeletionInstructions />} />
+                <Route path="/about-magnus" element={<AboutMagnus />} />
+              </Routes>
+            </header>
+          </div>
+        </main>
+      </Router>
     </Web3Provider>
   );
 };
