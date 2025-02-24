92932d3ade5ef8b74882cb0eb1b9c3aadae91cd7
Rohan <rohan.rana@digimantra.com>
2025-02-24 11:15:17 +0530

    352 hotfix upped api key expiration

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -82,7 +82,7 @@ export const data = defineData({
     defaultAuthorizationMode: 'apiKey',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+      expiresInDays: 120,
     },
   },
 });
